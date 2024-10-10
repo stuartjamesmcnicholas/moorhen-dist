2f84704c154b70d9fdef9e9954133857889f43a4
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-10-10 09:41:35 +0100

    Remove some unwanted imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useRef, useState, useReducer } from 'react';
-import { useEffect } from 'react';
-import { MoorhenContainer, MoorhenMolecule, MoorhenMap, MoorhenMoleculeSelect, addMolecule, MoorhenDraggableModalBase, addMap, setActiveMap, MoorhenReduxStore } from 'moorhen';
-import { InputGroup, Modal, NavDropdown } from 'react-bootstrap';
-import { Avatar, Button, Link, MenuItem, Typography } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux'
-import { Provider } from 'react-redux';
+import { useDispatch, Provider } from 'react-redux'
+import { MoorhenContainer, MoorhenReduxStore } from 'moorhen';
 
 import './App.css';
 import './moorhen.css';
