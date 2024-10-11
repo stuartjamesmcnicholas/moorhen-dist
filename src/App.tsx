738e1539741a46c78540354727cf77044f80667c
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-10-11 15:04:57 +0100

    Super minimal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
-import { useRef, useState } from 'react';
-import { useDispatch, Provider } from 'react-redux'
+import { Provider } from 'react-redux'
 import { MoorhenContainer, MoorhenReduxStore } from 'moorhen';
 
 import './App.css';
 import './moorhen.css';
 
-export const MyMoorhenContainer = (props) => {
-
-  const glRef = useRef(null)
-  const [appTitle, setAppTitle] = useState('MoorhenElectron')
-
-  const commandCentre = useRef(null)
-
-  const moleculesRef = useRef(null)
-  const mapsRef = useRef(null)
-
-  const dispatch = useDispatch()
-
-  const collectedProps = {
-        glRef,  commandCentre, moleculesRef, mapsRef
-  }
-
-    return (
-        <MoorhenContainer {...collectedProps}/>
-        )
-
-}
-
 function App() {
   return (
     <div className="App">
       <Provider store={MoorhenReduxStore}>
-        <MyMoorhenContainer/>
+        <MoorhenContainer/>
       </Provider>
     </div>
   );
