245d1c61d9ac6602199bedcbd3b32a7be6b890ab
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-10-22 16:26:35 +0100

    Work with moorhen 0.13.1-beta.0

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,24 @@ import './App.css';
 import './moorhen.css';
 
 function App() {
+  // Set urlPrefix to whereever your Moorhen will be in server root + /baby-gru
+  // e.g. If you are serving like:
+  //
+  // http://localhost:8000/foo/bar/sna/fu/
+  //
+  // then do:
+  //
+  // urlPrefix = "/foo/bar/sna/fu/baby-gru"
+
+  const urlPrefix = "/baby-gru"
+  const collectedProps = {
+         urlPrefix
+  }
+
   return (
     <div className="App">
       <Provider store={MoorhenReduxStore}>
-        <MoorhenContainer/>
+        <MoorhenContainer {...collectedProps} />
       </Provider>
     </div>
   );
